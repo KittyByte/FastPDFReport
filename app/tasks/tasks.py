4e3e3e656473d0dd0d99ee760e6da6d8ed24c3c5
KittyBite <oim@chatapp.online>
2025-11-29 19:36:13 +0300

    рефактор

--- app/tasks/tasks.py
+++ app/tasks/tasks.py
@@ -1,12 +1,11 @@
 import time
-from datetime import date
 from app.services.generate_pdf_report import generate_report_pdf
 from sqlalchemy import and_
 import logging
 
 from app.celery import celery_app
 from app.pdf_reports.dao import ReportDAO, SalesReportDAO
-from app.pdf_reports.models import ReportOrm, SalesReportOrm, ReportStatusEnum
+from app.pdf_reports.models import SalesReportOrm, ReportStatusEnum
 from app.pdf_reports.schemas import CreatePDFSchema
 from app.tasks.service import send_doc_and_msg_to_tg_bot
 from app.users.dao import UserDAO
@@ -35,8 +34,6 @@ def create_and_send_report(self, user_id: int, data: dict):
     )
 
     file_path = generate_report_pdf(sales)
-    # task['file_path'] = file_path
-    # task['status'] = ReportStatusEnum.done
     ReportDAO.update(task.id, file_path=str(file_path), status=ReportStatusEnum.done)
 
     logging.info(f"End create_and_send_report task for {user_id=} with {data=}")
